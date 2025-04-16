60404e378d392dde62ea4c75fe4356ce24c89975
MaNn0 <mlolo80013@gmail.com>
2025-04-16 14:12:13 +0400

    Ready for Railway

--- backend/app/core/cors.py
+++ backend/app/core/cors.py
@@ -3,7 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 def setup_cors(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["http://localhost:5174"], 
+        # Allow ALL origins (temporary solution)
+        allow_origins=["*"],  
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
